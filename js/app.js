1daabe34b847be243bf7b8479a42b80970adbbca
gererocharisse <91642206+gererocharisse@users.noreply.github.com>
2021-10-30 23:34:16 +0800

    Validating Fields

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-const sendBtn = document.getElementById('sendBtn');
-
+const sendBtn = document.getElementById('sendBtn'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
 
 
 
@@ -9,6 +11,11 @@ eventListeners();
 
 function eventListeners() {
     document.addEventListeners('DOMContentLoaded', appInit);
+
+
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 
@@ -18,4 +25,37 @@ function eventListeners() {
 
 function appInit() {
     sendBtn.disabled = true;
+}
+
+function validateField() {
+    let errors;
+
+    validateLength(this);
+
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+
+function validateLength(field) {
+    if(field.value.length > 0 ) {
+        field.style.borderButtonColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderButtonColor = 'red';
+        field.classList.add('error');
+    }
+
+}
+
+function validateEmail(field) {
+    let emailText = field.value;
+
+    if(emailText.indexOf('0') !== -1) {
+        field.style.borderButtonColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderButtonColor = 'red';
+        field.classList.add('error');
+    }
 }
